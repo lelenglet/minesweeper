ba56d1747bd53aa3fb18c5c4905f1d1b5b89af5d
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-04 09:11:32 +0200

    [+] opposite in Direction

--- src/Direction.java
+++ src/Direction.java
@@ -7,4 +7,27 @@ public enum Direction {
   NORTH_WEST,
   SOUTH_EAST,
   SOUTH_WEST;
+
+  public static Direction opposite(Direction d) {
+    switch (d) {
+      case NORTH:
+        return SOUTH;
+      case SOUTH:
+        return NORTH;
+      case EAST:
+        return WEST;
+      case WEST:
+        return EAST;
+      case NORTH_EAST:
+        return SOUTH_WEST;
+      case NORTH_WEST:
+        return SOUTH_EAST;
+      case SOUTH_EAST:
+        return NORTH_WEST;
+      case SOUTH_WEST:
+        return NORTH_EAST;
+      default:
+        return null;
+    }
+  }
 }
