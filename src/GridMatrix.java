42f0ec2d1bf4603d1d3708bc35fdb3c7c27861de
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-04 11:41:26 +0200

    [+] Class GridMatrix

---
+++ src/GridMatrix.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GridMatrix implements Matrix<Cell> {
+  private final List<List<Cell>> matrix;
+
+  public GridMatrix(final int nbRows, final int nbColumns) {
+    matrix = (List<List<Cell>>) new ArrayList<List<Cell>>();
+    for (int i = 0; i < nbRows; i++) {
+      matrix.add(new ArrayList<Cell>());
+      for (int j = 0; j < nbColumns; j++) {
+        matrix.get(i).add(null);
+      }
+    }
+  }
+
+  public Cell get(final int row, final int column) {
+    return matrix.get(row).get(column);
+  }
+
+  public void set(final int row, final int column, final Cell cell) {
+    matrix.get(row).set(column, cell);
+  }
+
+  public int getNbRows() {
+    return matrix.size();
+  }
+
+  public int getNbColumns() {
+    return matrix.get(0).size();
+  }
+}
