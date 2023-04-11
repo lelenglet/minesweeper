143eb3c800d20728e3337abadd7044488cd04da0
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-12 00:29:43 +0200

    [+] Javadoc for PlateMatrix.java (not finished)

--- src/PlateMatrix.java
+++ src/PlateMatrix.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * PlateMatrix implements a matrix of Cells.
+ */
 public class PlateMatrix implements Matrix<Cell> {
   private final List<List<Cell>> matrix;
 
-  public PlateMatrix(final int nbRows, final int nbColumns) {
+  /**
+   * Creates a matrix of nbRows rows and nbColumns columns.
+   *
+   * @param nbRows the number of rows
+   * @param nbColumns the number of columns
+   */
+  PlateMatrix(final int nbRows, final int nbColumns) {
     matrix = (List<List<Cell>>) new ArrayList<List<Cell>>();
     for (int i = 0; i < nbRows; i++) {
       matrix.add(new ArrayList<Cell>());
@@ -14,10 +23,24 @@ public class PlateMatrix implements Matrix<Cell> {
     }
   }
 
+  /**
+   * Returns the cell at the specified position in this matrix.
+   *
+   * @param row the row index of the cell to return
+   * @param column the column index of the cell to return
+   * @return the cell at the specified position in this matrix
+   */
   public Cell get(final int row, final int column) {
     return matrix.get(row).get(column);
   }
 
+  /**
+   * Set the cell at the specified position in this matrix with the specified cell.
+   *
+   * @param row the row index of the position for the cell to set
+   * @param column the column index of the position for the cell to set
+   * @param cell the cell to be stored at the specified position
+   */
   public void set(final int row, final int column, final Cell cell) {
     matrix.get(row).set(column, cell);
   }
