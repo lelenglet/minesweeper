26aca2ecd6680ceb24292106689cba54236ccafd
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-15 03:40:22 +0200

    [~] Optimisations + début Javadoc

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -4,21 +4,22 @@ import java.io.*;
 import javax.swing.*;
 
 public class MineSweeper implements MouseListener {
+  public static void main(final String[] args) {
+    final MineSweeper game = new MineSweeper();
+    game.showMenu();
+  }
+
   JFrame frame;
   Plate gamePlate;
   JButton[][] cellButtons;
   Container pane;
+
   Color[] colors = {
       new Color(187, 225, 182), // vert clair
       new Color(216, 226, 188), new Color(237, 227, 194), new Color(255, 221, 200),
       new Color(255, 188, 161), new Color(255, 156, 123), new Color(255, 125, 85),
       new Color(236, 87, 61), new Color(208, 36, 46) // rouge clair
   };
-
-  public static void main(final String[] args) {
-    final MineSweeper game = new MineSweeper();
-    game.showMenu();
-  }
 
   MineSweeper() {
     this.frame = new JFrame("Minesweeper - Antoine Chevalier | Léa Lenglet");
@@ -30,10 +31,54 @@ public class MineSweeper implements MouseListener {
     this.pane = frame.getContentPane();
   }
 
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    if (e.getButton() == MouseEvent.BUTTON1) {
+      for (int i = 0; i < gamePlate.getNbRows(); i++) {
+        for (int j = 0; j < gamePlate.getNbColumns(); j++) {
+          if (e.getSource() == cellButtons[i][j]) { // Si le bouton cliqué est le courant
+            boolean explode = gamePlate.getCell(i, j).uncover();
+            displayGrid();
+            if (explode) {
+              gameLost();
+            } else if (gamePlate.checkWin()) {
+              gameWon();
+            }
+          }
+        }
+      }
+    } else if (e.getButton() == MouseEvent.BUTTON3) {
+      for (int i = 0; i < gamePlate.getNbRows(); i++) {
+        for (int j = 0; j < gamePlate.getNbColumns(); j++) {
+          if (e.getSource() == cellButtons[i][j]) { // Si le bouton cliqué est le courant
+            gamePlate.getCell(i, j).toggleFlagged();
+            displayGrid();
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+  }
+
   /**
-   * Clear all elements on the frame
+   * Clear all elements on the frame.
    */
-  public void clearScreen() {
+  private void clearScreen() {
     pane.removeAll();
     pane.setVisible(false);
     pane.setVisible(true);
@@ -41,7 +86,134 @@ public class MineSweeper implements MouseListener {
   }
 
   /**
-   * Display main menu of the game
+   * Fonction that allow user to enter grid's parameters and init it.
+   */
+  private void newGame() {
+    int nbRows;
+    int nbColumns;
+    int minesPercentage;
+
+    do {
+      nbRows = this.askInt("Enter the number of rows (max 30):");
+      if (nbRows == Integer.MAX_VALUE) {
+        return;
+      }
+    } while (nbRows <= 0 || nbRows > 30);
+
+    do {
+      nbColumns = this.askInt("Enter the number of columns (max 30):");
+      if (nbColumns == Integer.MAX_VALUE) {
+        return;
+      }
+    } while (nbColumns <= 0 || nbColumns > 30);
+
+    do {
+      minesPercentage = this.askInt(
+          "Enter the percentage of mines in the grid (between 10 and 50):");
+      if (minesPercentage == Integer.MAX_VALUE) {
+        return;
+      }
+    } while (minesPercentage < 10 || minesPercentage > 50);
+
+    gamePlate = new Plate(nbRows, nbColumns, minesPercentage);
+    initButtons(nbRows, nbColumns);
+    this.gamePlate.startChrono();
+    displayGrid();
+  }
+
+  /**
+   * Initialize the grid of buttons and set addActionListener.
+   */
+  private void initButtons(int row, int column) {
+    this.cellButtons = new JButton[row][column];
+    for (int i = 0; i < cellButtons.length; i++) {
+      for (int j = 0; j < cellButtons[0].length; j++) {
+        cellButtons[i][j] = new JButton();
+        cellButtons[i][j].addMouseListener(this);
+        cellButtons[i][j].setBackground(Color.white);
+      }
+    }
+  }
+
+  /**
+   * Display the grid in game's window.
+   */
+  private void displayGrid() {
+    clearScreen();
+    pane.setLayout(new GridLayout(gamePlate.getNbRows() + 2, gamePlate.getNbColumns() + 2));
+
+    for (int j = 0; j < gamePlate.getNbColumns() + 2; j++) {
+      pane.add(new JLabel());
+    }
+    for (int i = 0; i < gamePlate.getNbRows(); i++) {
+      pane.add(new JLabel());
+      for (int j = 0; j < gamePlate.getNbColumns(); j++) {
+        if (gamePlate.getCell(i, j).isUncovered()) {
+          if (gamePlate.getCell(i, j).isMine()) {
+            cellButtons[i][j].setBackground(Color.red);
+            cellButtons[i][j].setEnabled(false);
+          } else {
+            cellButtons[i][j].setBackground(colors[gamePlate.getCell(i, j).getValue()]);
+            cellButtons[i][j].setEnabled(false);
+          }
+        }
+        cellButtons[i][j].setText(gamePlate.getCell(i, j).toString());
+        cellButtons[i][j].setFont(new Font("Arial", Font.PLAIN, 25));
+        pane.add(cellButtons[i][j]);
+      }
+      pane.add(new JLabel());
+    }
+
+    for (int j = 0; j < (gamePlate.getNbColumns() + 2) / 2 - 1; j++) {
+      pane.add(new JLabel());
+    }
+    Box box = Box.createHorizontalBox();
+    box.add(Box.createHorizontalGlue());
+    JButton save = new JButton("Save");
+    save.setSize(new Dimension(50, 25));
+    save.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        saveGame();
+      }
+    });
+    box.add(save);
+    pane.add(box, BorderLayout.SOUTH);
+    for (int j = 0; j < (gamePlate.getNbColumns() + 2) / 2 - 1; j++) {
+      pane.add(new JLabel());
+    }
+  }
+
+  private void gameLost() {
+    gamePlate.revealAll();
+    displayGrid();
+    JOptionPane.showMessageDialog(frame, "You lost !");
+    showMenu();
+  }
+
+  private void gameWon() {
+    JOptionPane.showMessageDialog(frame, "You won in "
+        + this.gamePlate.getTime() / 1000 + " seconds!");
+    showMenu();
+  }
+
+  private int askInt(String message) {
+    String input;
+
+    input = JOptionPane.showInputDialog(message);
+    if (input == null) {
+      return Integer.MAX_VALUE;
+    } else {
+      try {
+        return Integer.parseInt(input);
+      } catch (NumberFormatException e) {
+        JOptionPane.showMessageDialog(frame, "Invalid type of value !");
+        return askInt(message);
+      }
+    }
+  }
+
+  /**
+   * Display main menu of the game.
    */
   private void showMenu() {
     clearScreen();
@@ -104,83 +276,13 @@ public class MineSweeper implements MouseListener {
   }
 
   /**
-   * Fonction that allow user to enter the size of the grid
-   */
-  public void newGame() {
-    boolean cancel = false;
-    int nbRows = 0;
-    int nbColumns = 0;
-    int minesPercentage = 0;
-    String input;
-
-    while (cancel == false && (nbRows <= 0 || nbRows > 30)) {
-      input = JOptionPane.showInputDialog("Enter the number of rows (max 30):");
-      if (input == null) {
-        cancel = true;
-        break;
-      } else {
-        try {
-          nbRows = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-          JOptionPane.showMessageDialog(frame, "Invalid type of value !");
-        }
-      }
-    }
-    while (cancel == false && (nbColumns <= 0 || nbColumns > 30)) {
-      input = JOptionPane.showInputDialog("Enter the number of columns (max 30):");
-      if (input == null) {
-        cancel = true;
-        break;
-      }
-      try {
-        nbColumns = Integer.parseInt(input);
-      } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(frame, "Invalid type of value !");
-      }
-    }
-
-    while (cancel == false && (minesPercentage < 10 || minesPercentage > 50)) {
-      input = JOptionPane.showInputDialog(
-          "Enter the percentage of mines in the grid (between 10 and 50): ");
-      if (input == null) {
-        cancel = true;
-
-        break;
-      }
-      try {
-        minesPercentage = Integer.parseInt(input);
-      } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(frame, "Invalid type of value !");
-      }
-    };
-    if (cancel == false) {
-      gamePlate = new Plate(nbRows, nbColumns, minesPercentage);
-      initButtons(nbRows, nbColumns);
-      displayGrid();
-    }
-  }
-
-  /**
-   * Initialize the grid of buttons and set addActionListener
-   */
-  public void initButtons(int rows, int columns) {
-    this.cellButtons = new JButton[rows][columns];
-    for (int i = 0; i < cellButtons.length; i++) {
-      for (int j = 0; j < cellButtons[0].length; j++) {
-        cellButtons[i][j] = new JButton();
-        cellButtons[i][j].addMouseListener(this);
-        cellButtons[i][j].setBackground(Color.white);
-      }
-    }
-  }
-
-  /**
    * Save the gamePlate in a file
    */
   private void saveGame() {
     String m = JOptionPane.showInputDialog("Enter a file name : ");
-    try (ObjectOutputStream outputStream =
-             new ObjectOutputStream(new FileOutputStream(m + ".ser"))) {
+    try (ObjectOutputStream outputStream = new ObjectOutputStream(
+        new FileOutputStream(m + ".ser"))) {
+      this.gamePlate.stopChrono();
       outputStream.writeObject(gamePlate);
     } catch (IOException e) {
       JOptionPane.showMessageDialog(frame, "Failed to save the object to file");
@@ -193,113 +295,14 @@ public class MineSweeper implements MouseListener {
   private void loadGame() {
     String m = JOptionPane.showInputDialog("Enter a file name : ");
     try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(m + ".ser"))) {
-      Plate objet = (Plate) inputStream.readObject();
-      this.gamePlate = objet;
+      Plate loadedGamePlate = (Plate) inputStream.readObject();
+      this.gamePlate = loadedGamePlate;
       initButtons(gamePlate.getNbRows(), gamePlate.getNbColumns());
+      this.gamePlate.startChrono();
       displayGrid();
     } catch (ClassNotFoundException | IOException e) {
       JOptionPane.showMessageDialog(frame, "There is no save to load !");
       showMenu();
     }
   }
-
-  /**
-   * Display the grid in game's window
-   */
-  public void displayGrid() {
-    clearScreen();
-    pane.setLayout(new GridLayout(gamePlate.getNbRows() + 2, gamePlate.getNbColumns() + 2));
-
-    for (int j = 0; j < gamePlate.getNbColumns() + 2; j++) {
-      pane.add(new JLabel());
-    }
-    for (int i = 0; i < gamePlate.getNbRows(); i++) {
-      pane.add(new JLabel());
-      for (int j = 0; j < gamePlate.getNbColumns(); j++) {
-        if (gamePlate.getCell(i, j).isUncovered()) {
-          if (gamePlate.getCell(i, j).isMine()) {
-            cellButtons[i][j].setBackground(Color.red);
-            cellButtons[i][j].setEnabled(false);
-          } else {
-            cellButtons[i][j].setBackground(colors[gamePlate.getCell(i, j).getValue()]);
-            cellButtons[i][j].setEnabled(false);
-          }
-        }
-        cellButtons[i][j].setText(gamePlate.getCell(i, j).toString());
-        cellButtons[i][j].setFont(new Font("Arial", Font.PLAIN, 25));
-        pane.add(cellButtons[i][j]);
-      }
-      pane.add(new JLabel());
-    }
-
-    for (int j = 0; j < (gamePlate.getNbColumns() + 2) / 2 - 1; j++) {
-      pane.add(new JLabel());
-    }
-    Box box = Box.createHorizontalBox();
-    box.add(Box.createHorizontalGlue());
-    JButton save = new JButton("Save");
-    save.setSize(new Dimension(50, 25));
-    save.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        saveGame();
-      }
-    });
-    box.add(save);
-    pane.add(box, BorderLayout.SOUTH);
-    for (int j = 0; j < (gamePlate.getNbColumns() + 2) / 2 - 1; j++) {
-      pane.add(new JLabel());
-    }
-  }
-
-  public void gameLost() {
-    gamePlate.revealAll();
-    displayGrid();
-    JOptionPane.showMessageDialog(frame, "You lost !");
-    showMenu();
-  }
-
-  public void gameWon() {
-    JOptionPane.showMessageDialog(frame, "You won !");
-    showMenu();
-  }
-
-  @Override
-  public void mouseClicked(MouseEvent e) {
-    if (e.getButton() == MouseEvent.BUTTON1) {
-      for (int i = 0; i < gamePlate.getNbRows(); i++) {
-        for (int j = 0; j < gamePlate.getNbColumns(); j++) {
-          if (e.getSource() == cellButtons[i][j]) { // Si le bouton cliqué est le courant
-            boolean explode = gamePlate.getCell(i, j).uncover();
-            displayGrid();
-            if (explode) {
-              gameLost();
-            } else if (gamePlate.checkWin()) {
-              gameWon();
-            }
-          }
-        }
-      }
-    } else if (e.getButton() == MouseEvent.BUTTON3) {
-      for (int i = 0; i < gamePlate.getNbRows(); i++) {
-        for (int j = 0; j < gamePlate.getNbColumns(); j++) {
-          if (e.getSource() == cellButtons[i][j]) { // Si le bouton cliqué est le courant
-            gamePlate.getCell(i, j).toggleFlagged();
-            displayGrid();
-          }
-        }
-      }
-    }
-  }
-
-  @Override
-  public void mouseEntered(MouseEvent e) {}
-
-  @Override
-  public void mouseExited(MouseEvent e) {}
-
-  @Override
-  public void mousePressed(MouseEvent e) {}
-
-  @Override
-  public void mouseReleased(MouseEvent e) {}
 }
--- src/Plate.java
+++ src/Plate.java
@@ -1,15 +1,33 @@
-import java.io.*;
+import java.io.Serializable;
 import java.util.Random;
 
+/**
+ * The Plate class is used to create a new game.
+ * It contains the grid of cells and the timer.
+ */
 public class Plate implements Serializable {
+  public Timer chrono;
   public final PlateMatrix matrix;
 
   Plate(final int nbRows, final int nbColumns, final int minesPercentage) {
     this.matrix = new PlateMatrix(nbRows, nbColumns);
+    this.chrono = new Timer();
     this.addCells();
     this.connectAllCells();
     this.setMines((nbRows * nbColumns) * minesPercentage / 100);
     this.updateCellsValues();
+  }
+
+  public void stopChrono() {
+    this.chrono.stop();
+  }
+
+  public void startChrono() {
+    this.chrono.start();
+  }
+
+  public long getTime() {
+    return this.chrono.getElapsedTime();
   }
 
   public Cell getCell(final int x, final int y) {
@@ -24,6 +42,65 @@ public class Plate implements Serializable {
     return this.matrix.getNbColumns();
   }
 
+  /**
+   * Check the victory conditions.
+   * 
+   * @return true if it's won
+   */
+  public boolean checkWin() {
+    for (int i = 0; i < this.matrix.getNbRows(); i++) {
+      for (int j = 0; j < this.matrix.getNbColumns(); j++) {
+        if (!this.matrix.get(i, j).isMine() && this.matrix.get(i, j).isCovered()) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Reveal all the cells.
+   */
+  public void revealAll() {
+    for (int i = 0; i < this.matrix.getNbRows(); i++) {
+      for (int j = 0; j < this.matrix.getNbColumns(); j++) {
+        this.matrix.get(i, j).uncover();
+      }
+    }
+  }
+
+  /**
+   * Set mines randomly on the grid.
+   * 
+   * @param nbMines the number of mines
+   */
+  private void setMines(final int nbMines) {
+    final Random random = new Random();
+    int nbMinesToPlace = nbMines;
+    while (nbMinesToPlace > 0) {
+      final int x = random.nextInt(this.matrix.getNbRows());
+      final int y = random.nextInt(this.matrix.getNbColumns());
+      if (!this.matrix.get(x, y).isMine()) {
+        this.matrix.get(x, y).setMine();
+        nbMinesToPlace--;
+      }
+    }
+  }
+
+  /**
+   * Update the value of all cells.
+   */
+  private void updateCellsValues() {
+    for (int i = 0; i < this.matrix.getNbRows(); i++) {
+      for (int j = 0; j < this.matrix.getNbColumns(); j++) {
+        this.matrix.get(i, j).setValue();
+      }
+    }
+  }
+
+  /**
+   * Create all cells and add them to the grid.
+   */
   private void addCells() {
     for (int i = 0; i < this.matrix.getNbRows(); i++) {
       for (int j = 0; j < this.matrix.getNbColumns(); j++) {
@@ -32,6 +109,9 @@ public class Plate implements Serializable {
     }
   }
 
+  /**
+   * Connect all cells to their neighbors.
+   */
   private void connectAllCells() {
     for (int i = 0; i < this.matrix.getNbRows(); i++) {
       for (int j = 0; j < this.matrix.getNbColumns(); j++) {
@@ -47,46 +127,6 @@ public class Plate implements Serializable {
         if (j < this.matrix.getNbColumns() - 1) {
           this.matrix.get(i, j).connect(this.matrix.get(i, j + 1), Direction.EAST);
         }
-      }
-    }
-  }
-
-  private void setMines(int nbMines) {
-    int cpt = 0;
-    final Random r = new Random();
-    while (cpt < nbMines) {
-      final int x = r.nextInt(this.matrix.getNbRows());
-      final int y = r.nextInt(this.matrix.getNbColumns());
-      if (this.matrix.get(x, y).getValue() == 0) {
-        this.matrix.get(x, y).setMine();
-        cpt++;
-      }
-    }
-  }
-
-  private void updateCellsValues() {
-    for (int i = 0; i < this.matrix.getNbRows(); i++) {
-      for (int j = 0; j < this.matrix.getNbColumns(); j++) {
-        this.matrix.get(i, j).setValue();
-      }
-    }
-  }
-
-  public boolean checkWin() {
-    for (int i = 0; i < this.matrix.getNbRows(); i++) {
-      for (int j = 0; j < this.matrix.getNbColumns(); j++) {
-        if (this.matrix.get(i, j).getValue() > 0 && this.matrix.get(i, j).isCovered()) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  public void revealAll() {
-    for (int i = 0; i < this.matrix.getNbRows(); i++) {
-      for (int j = 0; j < this.matrix.getNbColumns(); j++) {
-        this.matrix.get(i, j).uncover();
       }
     }
   }
