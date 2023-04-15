edb5a3e8d5fccf73dec4fe1e3ea2d6de215810a8
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-15 02:48:57 +0200

    [+] Javadoc for Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,13 @@
 import java.io.Serializable;
 import java.util.EnumMap;
 
+/**
+ * The Cell class is used to create a new cell.
+ * A cell is a square on the grid of the game.
+ * It contains a value (number of mines in the neighborhood) and a state
+ * (covered, uncovered, flagged).
+ * It also contains a neighborhood (cells in the 8 directions).
+ */
 public class Cell implements Serializable {
   private int value;
   private State state;
@@ -11,10 +18,16 @@ public class Cell implements Serializable {
     this.neighborhood = new EnumMap<Direction, Cell>(Direction.class);
   }
 
+  /**
+   * Set the value of the cell to -1.
+   */
   public void setMine() {
     this.value = -1;
   }
 
+  /**
+   * Set the value of the cell based on the number of mines in the neighborhood.
+   */
   public void setValue() {
     if (!this.isMine()) {
       for (final Direction d : this.neighborhood.keySet()) {
@@ -25,30 +38,56 @@ public class Cell implements Serializable {
     }
   }
 
+  /**
+   * Get the value of the cell.
+   * 
+   * @return the value
+   */
   public int getValue() {
     return this.value;
   }
 
-  private State getState() {
-    return this.state;
-  }
-
+  /**
+   * Check if the cell is covered.
+   * 
+   * @return true if the cell is covered
+   */
   public boolean isCovered() {
     return this.getState() == State.COVERED;
   }
 
+  /**
+   * Check if the cell is uncovered.
+   * 
+   * @return true if the cell is uncovered
+   */
   public boolean isUncovered() {
     return this.getState() == State.UNCOVERED;
   }
 
+  /**
+   * Check if the cell is flagged.
+   * 
+   * @return true if the cell is flagged
+   */
   public boolean isFlagged() {
     return this.getState() == State.FLAGGED;
   }
 
+  /**
+   * Check if the cell is a mine.
+   * 
+   * @return true if the cell is a mine
+   */
   public boolean isMine() {
     return this.getValue() == -1;
   }
 
+  /**
+   * Get a string representation of the cell.
+   * 
+   * @return the string representation
+   */
   public String toString() {
     return switch (this.getState()) {
       case COVERED -> new String();
@@ -60,6 +99,9 @@ public class Cell implements Serializable {
     };
   }
 
+  /**
+   * Toggle the state of the cell between COVERED and FLAGGED.
+   */
   public void toggleFlagged() {
     this.state = switch (this.getState()) {
       case FLAGGED -> State.COVERED;
@@ -68,6 +110,12 @@ public class Cell implements Serializable {
     };
   }
 
+  /**
+   * Connect the cell to a neighbor in the given direction.
+   * 
+   * @param neighbor the neighbor to connect
+   * @param dir      the direction of the neighbor
+   */
   public void connect(final Cell neighbor, final Direction dir) {
     this.neighborhood.put(dir, neighbor);
     if (neighbor.getNeighbor(Direction.opposite(dir)) == null) {
@@ -75,6 +123,11 @@ public class Cell implements Serializable {
     }
   }
 
+  /**
+   * Uncover the cell.
+   * 
+   * @return true if the cell is a mine
+   */
   public boolean uncover() {
     if (this.isCovered() && !this.isFlagged()) {
       this.state = State.UNCOVERED;
@@ -90,6 +143,21 @@ public class Cell implements Serializable {
     }
   }
 
+  /**
+   * Get the state of the cell.
+   * 
+   * @return the state
+   */
+  private State getState() {
+    return this.state;
+  }
+
+  /**
+   * Get the neighbor in the given direction.
+   * 
+   * @param d the direction of the neighbor
+   * @return the neighbor
+   */
   private Cell getNeighbor(final Direction d) {
     return this.neighborhood.get(d);
   }
