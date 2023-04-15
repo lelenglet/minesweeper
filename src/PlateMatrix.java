9a927555b850d665756e5da4848a88ebae369cd6
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-15 02:56:43 +0200

    [+] Javadoc for PlateMatrix.java

--- src/PlateMatrix.java
+++ src/PlateMatrix.java
@@ -11,7 +11,7 @@ public class PlateMatrix implements Matrix<Cell>, Serializable {
   /**
    * Creates a matrix of nbRows rows and nbColumns columns.
    *
-   * @param nbRows the number of rows
+   * @param nbRows    the number of rows
    * @param nbColumns the number of columns
    */
   PlateMatrix(final int nbRows, final int nbColumns) {
@@ -27,7 +27,7 @@ public class PlateMatrix implements Matrix<Cell>, Serializable {
   /**
    * Returns the cell at the specified position in this matrix.
    *
-   * @param row the row index of the cell to return
+   * @param row    the row index of the cell to return
    * @param column the column index of the cell to return
    * @return the cell at the specified position in this matrix
    */
@@ -36,20 +36,31 @@ public class PlateMatrix implements Matrix<Cell>, Serializable {
   }
 
   /**
-   * Set the cell at the specified position in this matrix with the specified cell.
+   * Set the cell at the specified position in this matrix with the specified
+   * cell.
    *
-   * @param row the row index of the position for the cell to set
+   * @param row    the row index of the position for the cell to set
    * @param column the column index of the position for the cell to set
-   * @param cell the cell to be stored at the specified position
+   * @param cell   the cell to be stored at the specified position
    */
   public void set(final int row, final int column, final Cell cell) {
     matrix.get(row).set(column, cell);
   }
 
+  /**
+   * Returns the number of rows of this matrix.
+   *
+   * @return the number of rows of this matrix
+   */
   public int getNbRows() {
     return matrix.size();
   }
 
+  /**
+   * Returns the number of columns of this matrix.
+   *
+   * @return the number of columns of this matrix
+   */
   public int getNbColumns() {
     return matrix.get(0).size();
   }
