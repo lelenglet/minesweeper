665fbb19bd3c75e4cd5632d2fd476aa960e3ee70
anchevalie7 <antoine.chevalier2@etu.uca.fr>
2023-04-15 02:45:09 +0200

    [+] Timer.java

---
+++ src/Timer.java
@@ -0,0 +1,39 @@
+import java.io.Serializable;
+
+/**
+ * This class is used to measure the time elapsed during a game.
+ */
+public class Timer implements Serializable {
+  private long elapsedTime;
+  private long begin;
+  private long end;
+
+  Timer() {
+    elapsedTime = 0;
+  }
+
+  /**
+   * Start the timer.
+   */
+  public void start() {
+    begin = System.currentTimeMillis();
+  }
+
+  /**
+   * Stop the timer.
+   */
+  public void stop() {
+    end = System.currentTimeMillis();
+    elapsedTime += (long) (end - begin);
+  }
+
+  /**
+   * Get the elapsed time.
+   * 
+   * @return the elapsed time
+   */
+  public long getElapsedTime() {
+    this.stop();
+    return elapsedTime;
+  }
+}
